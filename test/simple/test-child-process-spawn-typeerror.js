b868411759ff995a2a01325dacf074656e4b1889
Bert Belder <bertbelder@gmail.com>
2015-01-10 23:07:49 +0100

    Squashed commit of the following:
    commit 709558ce0d4027978a742c618d014e5713422263
    Author: cjihrig <cjihrig@gmail.com>
    Date:   Wed Dec 17 14:58:09 2014 -0500
    
        test: remove redundant code in test
    
        A block of asserts were duplicated in
        test/simple/test-child-process-spawn-typeerror.js. This commit
        removes the duplicated asserts.
    
        Fixes: https://github.com/joyent/node/pull/8454
        Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    
    commit 9e6d2792ae2eb3ad8553192a1d9c7a7d58808123
    Author: Trevor Norris <trev.norris@gmail.com>
    Date:   Tue Nov 18 16:42:10 2014 -0800
    
        lint: fix lint issues
    
        Forgot to fix these before landing the patch.
    
        Fixes: e17c5a72
    
    commit 756612776e6bb6b4329a132948f40ad9c28555e2
    Author: Sam Roberts <sam@strongloop.com>
    Date:   Wed Oct 8 13:30:38 2014 -0700
    
        test: test all spawn parameter positions
    
        PR-URL: https://github.com/joyent/node/pull/8454
        Reviewed-by: Trevor Norris <trev.norris@gmail.com>
    
    commit 408812653153cc50f626c7c69b2f8ea4a4be8cec
    Author: Sam Roberts <sam@strongloop.com>
    Date:   Mon Sep 29 11:26:43 2014 -0700
    
        child_process: check fork args is an array
    
        Optional fork args should be type-checked with same behaviour as the
        equivalent argument to spawn.
    
        PR-URL: https://github.com/joyent/node/pull/8454
        Reviewed-by: Trevor Norris <trev.norris@gmail.com>
    
    commit 11bff9beb2f837a923eb0e515331303f7771301e
    Author: Sam Roberts <sam@strongloop.com>
    Date:   Thu Sep 25 22:28:33 2014 -0700
    
        child_process: check execFile args is an array
    
        execFile and spawn have same API signature with respect to optional arg
        array and optional options object, they should have same behaviour with
        respect to argument validation.
    
        PR-URL: https://github.com/joyent/node/pull/8454
        Reviewed-by: Trevor Norris <trev.norris@gmail.com>
    
    commit d531ab78c41019f6fdcaf08a1e84ab2b9e439c1b
    Author: cjihrig <cjihrig@gmail.com>
    Date:   Wed Sep 17 17:26:46 2014 -0400
    
        child_process: properly support optional args
    
        Currently, a TypeError is incorrectly thrown if the second argument is
        an object. This commit allows the args argument to be properly omitted.
    
        Fixes: https://github.com/joyent/node/issues/6068
        Reviewed-by: Trevor Norris <trev.norris@gmail.com>

--- test/simple/test-child-process-spawn-typeerror.js
+++ test/simple/test-child-process-spawn-typeerror.js
@@ -19,24 +19,85 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var spawn = require('child_process').spawn,
-  assert = require('assert'),
-  windows = (process.platform === 'win32'),
-  cmd = (windows) ? 'ls' : 'dir',
-  errors = 0;
+var assert = require('assert');
+var child_process = require('child_process');
+var spawn = child_process.spawn;
+var fork = child_process.fork;
+var execFile = child_process.execFile;
+var cmd = (process.platform === 'win32') ? 'rundll32' : 'ls';
+var empty = require('../common').fixturesDir + '/empty.js';
 
-try {
-  // Ensure this throws a TypeError
-  var child = spawn(cmd, 'this is not an array');
+// Argument types for combinatorics
+var a=[], o={}, c=(function callback(){}), s='string', u=undefined, n=null;
 
-  child.on('error', function (err) {
-    errors++;
-  });
+// function spawn(file=f [,args=a] [, options=o]) has valid combinations:
+//   (f)
+//   (f, a)
+//   (f, a, o)
+//   (f, o)
+assert.doesNotThrow(function() { spawn(cmd); });
+assert.doesNotThrow(function() { spawn(cmd, a); });
+assert.doesNotThrow(function() { spawn(cmd, a, o); });
+assert.doesNotThrow(function() { spawn(cmd, o); });
 
-} catch (e) {
-  assert.equal(e instanceof TypeError, true);
-}
+// Variants of undefined as explicit 'no argument' at a position
+assert.doesNotThrow(function() { execFile(empty, u, o); });
+assert.doesNotThrow(function() { execFile(empty, a, u); });
+assert.doesNotThrow(function() { execFile(empty, n, o); });
+assert.doesNotThrow(function() { execFile(empty, a, n); });
 
-process.on('exit', function() {
-  assert.equal(errors, 0);
-});
+assert.throws(function() { spawn(cmd, s); }, TypeError);
+assert.doesNotThrow(function() { spawn(cmd, a, s); }, TypeError);
+
+
+// verify that execFile has same argument parsing behaviour as spawn
+//
+// function execFile(file=f [,args=a] [, options=o] [, callback=c]) has valid
+// combinations:
+//   (f)
+//   (f, a)
+//   (f, a, o)
+//   (f, a, o, c)
+//   (f, a, c)
+//   (f, o)
+//   (f, o, c)
+//   (f, c)
+assert.doesNotThrow(function() { execFile(cmd); });
+assert.doesNotThrow(function() { execFile(cmd, a); });
+assert.doesNotThrow(function() { execFile(cmd, a, o); });
+assert.doesNotThrow(function() { execFile(cmd, a, o, c); });
+assert.doesNotThrow(function() { execFile(cmd, a, c); });
+assert.doesNotThrow(function() { execFile(cmd, o); });
+assert.doesNotThrow(function() { execFile(cmd, o, c); });
+assert.doesNotThrow(function() { execFile(cmd, c); });
+
+// Variants of undefined as explicit 'no argument' at a position
+assert.doesNotThrow(function() { execFile(cmd, u, o, c); });
+assert.doesNotThrow(function() { execFile(cmd, a, u, c); });
+assert.doesNotThrow(function() { execFile(cmd, a, o, u); });
+assert.doesNotThrow(function() { execFile(cmd, n, o, c); });
+assert.doesNotThrow(function() { execFile(cmd, a, n, c); });
+assert.doesNotThrow(function() { execFile(cmd, a, o, n); });
+
+// string is invalid in arg position (this may seem strange, but is
+// consistent across node API, cf. `net.createServer('not options', 'not
+// callback')`
+assert.throws(function() { execFile(cmd, s, o, c); }, TypeError);
+assert.doesNotThrow(function() { execFile(cmd, a, s, c); });
+assert.doesNotThrow(function() { execFile(cmd, a, o, s); });
+
+
+// verify that fork has same argument parsing behaviour as spawn
+//
+// function fork(file=f [,args=a] [, options=o]) has valid combinations:
+//   (f)
+//   (f, a)
+//   (f, a, o)
+//   (f, o)
+assert.doesNotThrow(function() { fork(empty); });
+assert.doesNotThrow(function() { fork(empty, a); });
+assert.doesNotThrow(function() { fork(empty, a, o); });
+assert.doesNotThrow(function() { fork(empty, o); });
+
+assert.throws(function() { fork(empty, s); }, TypeError);
+assert.doesNotThrow(function() { fork(empty, a, s); }, TypeError);
